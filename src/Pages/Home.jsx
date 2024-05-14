25a0f613cc533e86c0ca0e370ab51a4ce9ac5a98
Gift Jacksun <giftjacksun@gmail.com>
2024-05-14 09:15:56 +0100

    animation

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
 import styles from "../styles/home.module.css";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Home = () => {
-    const [showInput, setShowInput] = useState(false);
-    
-    const toggleInput = () => {
-        setShowInput(!showInput)
-    }
+  const [showInput, setShowInput] = useState(false);
+
+  const toggleInput = () => {
+    setShowInput(!showInput);
+  };
+
+  const boxVars = {
+    initial: {
+      opacity: 0,
+      y: -10,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+    },
+    exit: {
+      opacity: 0,
+      y: -10,
+    },
+  };
 
   return (
     <>
@@ -26,19 +42,27 @@ const Home = () => {
 
         <div className={styles.wrapper}>
           {showInput ? (
-            <div className={styles.input_wrap}>
-              <textarea placeholder="Enter todo..."></textarea>
-              <div className={styles.btns}>
-                <button onClick={toggleInput} className={styles.close}>Close</button>
-                <button className={styles.add}>
-                  Add <i className="fa-solid fa-plus"></i>
-                </button>
-              </div>
-            </div>
+            <AnimatePresence mode="wait">
+              <motion.div
+                variants={boxVars}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                className={styles.input_wrap}
+              >
+                <textarea placeholder="Enter todo..."></textarea>
+                <div className={styles.btns}>
+                  <button onClick={toggleInput} className={styles.close}>
+                    Close
+                  </button>
+                  <button className={styles.add}>
+                    Add <i className="fa-solid fa-plus"></i>
+                  </button>
+                </div>
+              </motion.div>
+            </AnimatePresence>
           ) : (
-                          <button
-                              onClick={toggleInput}
-                              className={styles.toggle}>
+            <button onClick={toggleInput} className={styles.toggle}>
               Add task &nbsp; <i className="fa-solid fa-plus"></i>
             </button>
           )}
