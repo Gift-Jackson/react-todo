105ab026291f3016d3e656cd37afb892fed303ec
Gift Jacksun <giftjacksun@gmail.com>
2024-05-14 21:43:53 +0100

    Added todo, animation and toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./Pages/Home"
+import { Toaster } from 'sonner';
 
 const App = () => {
   return (
     <>
+      <Toaster position="top-center"
+      toastOptions={{
+        style: {
+          background: 'crimson',
+          border: 'none'
+        },
+        className: 'class',
+      }}/>
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,31 @@
 import { useState } from "react";
 import styles from "../styles/home.module.css";
 import { AnimatePresence, motion } from "framer-motion";
+import { toast } from "sonner";
 
 const Home = () => {
   const [showInput, setShowInput] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
+  const handleChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const addTodo = () => {
+    if (newTodo.trim() === "") {
+      toast.warning("Add a todo first!");
+    } else {
+      setTodos((t) => [...t, newTodo]);
+      setNewTodo("");
+      setShowInput(!showInput);
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodo = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodo);
+  };
   const toggleInput = () => {
     setShowInput(!showInput);
   };
@@ -17,6 +38,31 @@ const Home = () => {
     animate: {
       opacity: 1,
       y: 0,
+      transition: {
+        type: "spring",
+        stiffness: "400",
+      },
+    },
+    exit: {
+      opacity: 0,
+      y: -10,
+    },
+  };
+  const todoVars = {
+    initial: {
+      opacity: 0,
+      y: -10,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      staggerChildren: "0.2s",
+      transition: {
+        delay: 0.2,
+
+        type: "spring",
+        stiffness: "400",
+      },
     },
     exit: {
       opacity: 0,
@@ -29,7 +75,6 @@ const Home = () => {
       <main>
         <div className={styles.header}>
           <h1>Todo</h1>
-
           <button className={styles.menu_btn}>
             <span className="material-symbols-outlined">side_navigation</span>
           </button>
@@ -40,9 +85,34 @@ const Home = () => {
           <input type="text" placeholder="Search todo..." />
         </div>
 
+        <div>
+          <ul className={styles.todos}>
+            <AnimatePresence mode="popLayout">
+              {todos.map((todo, index) => (
+                <motion.li
+                  variants={todoVars}
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  className={styles.todo}
+                  key={index}
+                >
+                  <p>{todo}</p>
+                  <button
+                    className={styles.del}
+                    onClick={() => deleteTodo(index)}
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </motion.li>
+              ))}
+            </AnimatePresence>
+          </ul>
+        </div>
+
         <div className={styles.wrapper}>
           {showInput ? (
-            <AnimatePresence mode="wait">
+            <AnimatePresence>
               <motion.div
                 variants={boxVars}
                 initial="initial"
@@ -50,21 +120,31 @@ const Home = () => {
                 exit="exit"
                 className={styles.input_wrap}
               >
-                <textarea placeholder="Enter todo..."></textarea>
+                <textarea
+                  rows={4}
+                  placeholder="Enter todo..."
+                  onChange={handleChange}
+                ></textarea>
                 <div className={styles.btns}>
                   <button onClick={toggleInput} className={styles.close}>
                     Close
                   </button>
-                  <button className={styles.add}>
+                  <button className={styles.add} onClick={addTodo}>
                     Add <i className="fa-solid fa-plus"></i>
                   </button>
                 </div>
               </motion.div>
             </AnimatePresence>
           ) : (
-            <button onClick={toggleInput} className={styles.toggle}>
-              Add task &nbsp; <i className="fa-solid fa-plus"></i>
-            </button>
+            <div className={styles.end}>
+              <motion.button
+                layout
+                onClick={toggleInput}
+                className={styles.toggle}
+              >
+                Add task &nbsp; <i className="fa-solid fa-plus"></i>
+              </motion.button>
+            </div>
           )}
         </div>
       </main>
