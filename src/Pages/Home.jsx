8feed7e15a02336bf6d83d7b380dfa6bd671487a
Gift Jacksun <giftjacksun@gmail.com>
2024-05-16 05:34:15 +0100

    Fixed bugs

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,8 +16,11 @@ const Home = () => {
 
   const handleChange = (e) => {
     setNewTodo(e.target.value);
-
+    if (newTodo.trim() === "") {
+      setNewLine(false)
+    }
   }
+
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       setNewLine(true);
@@ -25,146 +28,149 @@ const Home = () => {
   }
 
 
-    const addTodo = () => {
-      if (newTodo.trim() === "") {
-        toast.warning("Add a todo first!");
-      } else {
-        const newTodos = newTodo.split('\n').filter(todo => todo.trim() !== ''); // Filter out empty lines
-        setTodos((prevTodos) => [...prevTodos, ...newTodos]);
-        setNewTodo("");
-        setShowInput(!showInput);
-      }
-    };
 
-
-    const deleteTodo = (index) => {
-      const updatedTodo = todos.filter((_, i) => i !== index);
-      setTodos(updatedTodo);
-    };
-    const toggleInput = () => {
+  const addTodo = () => {
+    if (newTodo.trim() === "") {
+      toast.warning("Add a todo first!");
+    } else {
+      const newTodos = newTodo.split('\n').filter(todo => todo.trim() !== ''); // Filter out empty lines
+      setTodos((prevTodos) => [...prevTodos, ...newTodos]);
+      setNewTodo("");
       setShowInput(!showInput);
-    };
+      setNewLine(false)
+    }
 
-    const boxVars = {
-      initial: {
-        opacity: 0,
-        y: -10,
+  };
+
+
+  const deleteTodo = (index) => {
+    const updatedTodo = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodo);
+  };
+  const toggleInput = () => {
+    setShowInput(!showInput);
+  };
+
+  const boxVars = {
+    initial: {
+      opacity: 0,
+      y: -10,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: "700",
       },
-      animate: {
-        opacity: 1,
-        y: 0,
-        transition: {
-          type: "spring",
-          stiffness: "700",
-        },
+    },
+  };
+  const todoVars = {
+    initial: {
+      opacity: 0,
+      y: -10,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      staggerChildren: 0.3,
+      transition: {
+        delay: 0.2,
+        type: "spring",
+        stiffness: "700",
       },
-    };
-    const todoVars = {
-      initial: {
-        opacity: 0,
-        y: -10,
-      },
-      animate: {
-        opacity: 1,
-        y: 0,
-        staggerChildren: 0.3,
-        transition: {
-          delay: 0.2,
-          type: "spring",
-          stiffness: "700",
-        },
-      },
+    },
 
-    };
+  };
 
-    return (
-      <>
-        <main>
-          <div className={styles.header}>
-            <h1>Todo</h1>
-            <button className={styles.menu_btn}>
-              <span className="material-symbols-outlined">side_navigation</span>
-            </button>
-          </div>
+  return (
+    <>
+      <main>
+        <div className={styles.header}>
+          <h1>Todo</h1>
+          <button className={styles.menu_btn}>
+            <span className="material-symbols-outlined">side_navigation</span>
+          </button>
+        </div>
 
-          <div className={styles.search}>
-            <span className="material-symbols-outlined">search</span>
-            <input
-              type="text"
-              placeholder="Search todo..."
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-            />
-          </div>
-          <p className={styles.info}>Press Enter after each todo entry in the textarea to add more than one task.</p>
-          <div>
-            <ul className={styles.todos}>
-              <AnimatePresence mode="popLayout">
-                {filteredItems.map((todo, index) => (
-                  <motion.li
-                    variants={todoVars}
-                    initial="initial"
-                    animate="animate"
-                    exit="exit"
-                    className={styles.todo}
-                    key={index}
-                  >
-                    <p>{todo}</p>
-                    <button
-                      className={styles.del}
-                      onClick={() => deleteTodo(index)}
-                    >
-                      <i className="fa-solid fa-trash"></i>
-                    </button>
-                  </motion.li>
-                ))}
-              </AnimatePresence>
-            </ul>
-          </div>
-
-          <div className={styles.wrapper}>
-            {showInput ? (
-                <motion.div
-                  variants={boxVars}
+        <div className={styles.search}>
+          <span className="material-symbols-outlined">search</span>
+          <input
+            type="text"
+            placeholder="Search todo..."
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+        </div>
+        <p className={styles.info}>Press Enter after each todo entry in the textarea to add more than one task.</p>
+        <div>
+          <ul className={styles.todos}>
+            <AnimatePresence mode="popLayout">
+              {filteredItems.map((todo, index) => (
+                <motion.li
+                  variants={todoVars}
                   initial="initial"
                   animate="animate"
                   exit="exit"
-                  className={styles.input_wrap}
+                  className={styles.todo}
+                  key={index}
                 >
-                  <div className={styles.area}>
-                    <textarea
-                      rows={4}
-                      placeholder="Enter todo..."
-                      onChange={handleChange}
-                      onKeyDown={handleKeyPress}
-                    >
-                    </textarea>
-                    <span className={styles.tip}>{newLine ? "Multi-todo" : "Single todo"}</span>
-                  </div>
-                  <div className={styles.btns}>
-                    <button onClick={toggleInput} className={styles.close}>
-                      Close
-                    </button>
-                    <button className={styles.add} onClick={addTodo}>
-                      Add <i className="fa-solid fa-plus"></i>
-                    </button>
-                  </div>
-                </motion.div>
-            ) : (
-              <div className={styles.end}>
-                <motion.button
-                  layout
-                  onClick={toggleInput}
-                  className={styles.toggle}
-                >
-                  Add task &nbsp; <i className="fa-solid fa-plus"></i>
-                </motion.button>
-              </div>
-            )}
-          </div>
-        </main>
-      </>
-    );
-  };
+                  <p>{todo}</p>
+                  <button
+                    className={styles.del}
+                    onClick={() => deleteTodo(index)}
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </motion.li>
+              ))}
+            </AnimatePresence>
+          </ul>
+        </div>
 
-  export default Home;
+        <div className={styles.wrapper}>
+          {showInput ? (
+            <motion.div
+              variants={boxVars}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              className={styles.input_wrap}
+            >
+              <div className={styles.area}>
+                <textarea
+                  rows={4}
+                  placeholder="Enter todo..."
+                  onChange={handleChange}
+                  onKeyDown={handleKeyPress}
+                >
+                </textarea>
+                <span className={styles.tip}>{newLine ? "Multi-todo" : "Single todo"}</span>
+              </div>
+              <div className={styles.btns}>
+                <button onClick={toggleInput} className={styles.close}>
+                  Close
+                </button>
+                <button className={styles.add} onClick={addTodo}>
+                  Add <i className="fa-solid fa-plus"></i>
+                </button>
+              </div>
+            </motion.div>
+          ) : (
+            <div className={styles.end}>
+              <motion.button
+                layout
+                onClick={toggleInput}
+                className={styles.toggle}
+              >
+                Add task &nbsp; <i className="fa-solid fa-plus"></i>
+              </motion.button>
+            </div>
+          )}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
